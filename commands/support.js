54c4fd058ce2fd42d542360b6eb2bf1f33b8ebc8
mskstanmay <64010302+mskstanmay@users.noreply.github.com>
2020-11-11 18:32:56 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -6,7 +6,7 @@ module.exports.run = (bot,message,args)=>{
     const embed = new Discord.MessageEmbed()
     .setColor('RANDOM')
     .setTitle(`SUPPORT SERVER INVITE`)
-    .setDescription(`[CLICK HERE](https://discord.gg/UatfmrmT)`)
+    .setDescription(`[CLICK HERE](https://discord.gg/aimop)`)
     message.channel.send(embed);
 
 }
