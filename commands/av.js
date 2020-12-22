7c31ca2ee5c82e4f149e3bdba006e991de0dc269
mskstanmay <64010302+mskstanmay@users.noreply.github.com>
2020-12-22 16:48:58 +0530

    Add files via upload

--- commands/av.js
+++ commands/av.js
@@ -1,14 +1,18 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+const {Client , MessageEmbed} = require('discord.js');
+const bot = new Client();
 
 
 module.exports.run = (bot,message,args)=>{
-  
-    let target = message.guild.members.cache.get(message.mentions.users.first()) || bot.users.cache.get(args[1]) || message.member;
+    let target = message.mentions.members.first() || message.member
     if(!target) return message.reply(`No User Mentioned !`)
+   const avembed = new MessageEmbed()
+   .setTitle(`AVATAR OF ${target.user.tag}`)
+   .setColor('RANDOM')
+   .setImage(target.user.displayAvatarURL({dynamic : true , size: 4096 ,format:"png"}))
+   .setFooter(`Requested by ${message.author.tag}`)
+   .setTimestamp()
    
-   
-    message.channel.send(message.author.displayAvatarURL({dynamic : true}))
+    message.channel.send(avembed)
 }
 
 module.exports.help ={
