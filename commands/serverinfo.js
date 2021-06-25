593495c05e8a9aa68c6539a36b90fd0d23143d10
mskstanmay <64010302+mskstanmay@users.noreply.github.com>
2021-06-25 17:01:16 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -1,7 +1,4 @@
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-
-
 module.exports.run = (bot,message,args)=>{   
 let server = message.guild;
       let serverinfoembed = new Discord.MessageEmbed()
